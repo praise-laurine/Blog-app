12b318c3884be203f99d862bcc7d9c76b776880a
praise-laurine <praiselaurine10@gmail.com>
2020-12-13 01:45:26 +0300

    added an index function

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,18 @@
+from flask import render_template,request,url_for,redirect,flash,abort
+from . import main
+from ..models import *
+from .. import db
+
+
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Home - Welcome to my personal Blog'
+    quotes = get_quotes()
+    blogs = Blog.query.all()
+    return render_template('index.html', title = title, quote = quotes,blogs=blogs)
+
+
 
