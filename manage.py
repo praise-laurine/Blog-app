afb5ced23da7094c44697c3a32e6c01e461612df
praise-laurine <praiselaurine10@gmail.com>
2020-12-13 00:55:43 +0300

    add db migrate in the app instance

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
 from app.models import User
 
 
@@ -7,8 +8,8 @@ from app.models import User
 app = create_app('development')
 
 manager = Manager(app)
-
-manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.command
 def test():
@@ -19,7 +20,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User, Role = Role )
 
 if __name__ == '__main__':
     manager.run()
